8b7f2a44da326f63e5282a6e30fc84ba0def28b8
joaodematejr <joaodematejr@gmail.com>
2019-08-11 00:20:34 -0300

    Implementacao App

--- tindev/src/index.js
+++ tindev/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Routes from './routes';
 
 export default function App(){
   return (
-    <Text>Olá Mundo</Text>
+    <Routes/>
   );
 };
---
+++ tindev/src/routes.js
@@ -0,0 +1,10 @@
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import Login from './pages/Login';
+import Main from './pages/Main';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    Login,
+    Main
+  })
+);
