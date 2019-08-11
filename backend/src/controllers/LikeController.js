ad89d43d2c66ba46cff881f041ca83786cfb756e
joaodematejr <joaodematejr@gmail.com>
2019-08-11 17:35:05 -0300

    Finalização do projeto Web

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,9 +2,6 @@ const Dev = require("../models/Dev");
 
 module.exports = {
   async store(req, res) {
-    console.log(req.params.devId);
-    console.log(req.headers.user);
-
     const { user } = req.headers;
     const { devId } = req.params;
 
@@ -16,7 +13,17 @@ module.exports = {
     }
 
     if (targetDev.likes.includes(loggerDev._id)) {
-      console.log("Deu MATCH");
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit("match", targetDev);
+        console.log("targetDev", targetDev);
+      }
+      if (targetSocket) {
+        req.io.to(targetSocket).emit("match", loggerDev);
+        console.log("loggedSocket", loggerDev);
+      }
     }
 
     loggerDev.likes.push(targetDev._id);
